8e4291f6cac60cbaea13275fe1eb1e5a4498e482
Deep Dalal <deepdalal20@gmail.com>
2022-09-01 21:56:01 +0530

    01/09/2022 (Second)

--- loreg.php
+++ loreg.php
@@ -9,57 +9,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>    
-<a href="index.php"> <img class="btn"  src="seewans.png" alt="" width="72" height="57"> </a>
-<h1>Seewans Bakery</h1></h1>
-    <div class="container" id="container">
-	<div class="form-container sign-up-container">
-		<form action="cust.php">
-			<h1>Create Account</h1>
-			<input type="text" placeholder="Name" />
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<button>Sign Up</button>
-		</form>
-	</div>
-	<div class="form-container sign-in-container">
-		<form action="cust.php">
-			<h1>Sign in</h1>
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<a href="#">Forgot your password?</a>
-			<button>Sign In</button>
-		</form>
-	</div>
-	<div class="overlay-container">
-		<div class="overlay">
-			<div class="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal details</p>
-				<button class="ghost" id="signIn">Sign In</button>
-			</div>
-			<div class="overlay-panel overlay-right">
-				<h1>Hi There!</h1>
-				<p>Enter your personal details to open an account with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
+	<a href="index.php"> <img class="btn"  src="seewans.png" alt="" width="72" height="57"> </a>
+	<h1>Seewans Bakery</h1></h1>
+    	<div class="container" id="container">
+		<div class="form-container sign-up-container">
+			<form action="cust.php">
+				<h1>Create Account</h1>
+				<input type="text" placeholder="Name" required>
+				<input type="email" placeholder="Email" required>
+				<input type="password" placeholder="Password" required>
+				<button>Sign Up</button>
+			</form>
+		</div>
+		<div class="form-container sign-in-container">
+			<form action="cust.php">
+				<h1>Sign in</h1>
+				<input type="email" placeholder="Email" required>
+				<input type="password" placeholder="Password" required>
+				<a href="#">Forgot your password?</a>
+				<button>Sign In</button>
+			</form>
+		</div>
+		<div class="overlay-container">
+			<div class="overlay">
+				<div class="overlay-panel overlay-left">
+					<h1>Welcome Back!</h1>
+					<p>To keep connected with us please login with your personal details</p>
+					<button class="ghost" id="signIn">Sign In</button>
+				</div>
+				<div class="overlay-panel overlay-right">
+					<h1>Hi There!</h1>
+					<p>Enter your personal details to open an account with us</p>
+					<button class="ghost" id="signUp">Sign Up</button>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-  <script src="script.js" charset="utf-8"></script>
-  <script>
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
-
-    signUpButton.addEventListener('click', () => {
-	  container.classList.add("right-panel-active");
-    });
-
-    signInButton.addEventListener('click', () => {
-  	container.classList.remove("right-panel-active");
-  });
-</script>
-      <script src="https://kit.fontawesome.com/96531cd29f.js" crossorigin="anonymous"></script>
+  	<script src="script.js" charset="utf-8"></script>
+	<script>
+    	const signUpButton = document.getElementById('signUp');
+    	const signInButton = document.getElementById('signIn');
+    	const container = document.getElementById('container');
+		signUpButton.addEventListener('click', () => {
+		  container.classList.add("right-panel-active");
+    	});
+    	signInButton.addEventListener('click', () => {
+  			container.classList.remove("right-panel-active");
+  		});
+	</script>
+    <script src="https://kit.fontawesome.com/96531cd29f.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
 </body>

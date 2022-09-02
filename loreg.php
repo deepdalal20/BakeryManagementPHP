50a95fe9a5e975df8186273e097f3d13b15aa258
Deep Dalal <deepdalal20@gmail.com>
2022-09-02 18:55:25 +0530

    02/09/2022 (Second)

--- loreg.php
+++ loreg.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,78 +6,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" href="style2.css">
+    <title> Login/Register </title>
 </head>
 <body>
     <div class="container">
+    <a href="index.php"> <img class="btn"  src="seewans.png" alt="" width="72" height="57"> </a>
         <div class="forms">
             <div class="form login">
                 <span class="title">Login</span>
-
                 <form action="cust.php">
                     <div class="input-field">
-                        <input type="text" placeholder="Enter your email" required>
+                        <input type="email" placeholder="Enter your email" name="email" required>
                         <i class="uil uil-envelope icon"></i>
                     </div>
                     <div class="input-field">
-                        <input type="password" class="password" placeholder="Enter your password" required>
+                        <input type="password" class="password" placeholder="Enter your password" name="password" required>
                         <i class="uil uil-lock icon"></i>
                         <i class="uil uil-eye-slash showHidePw"></i>
                     </div>
-
                     <div class="checkbox-text">
                         <div class="checkbox-content">
                             <input type="checkbox" id="logCheck">
                             <label for="logCheck" class="text">Remember me</label>
                         </div>
-                        
                         <a href="#" class="text">Forgot password?</a>
                     </div>
-
                     <div class="input-field button">
-                        <input type="button" value="Login">
+                        <input type="submit" value="Login">
                     </div>
                 </form>
-
                 <div class="login-signup">
-                    <span class="text">Not a member?
+                    <span class="text">Not a user?
                         <a href="#" class="text signup-link">Signup Now</a>
                     </span>
                 </div>
             </div>
             <div class="form signup">
-                <span class="title">Registration</span>
-
+                <span class="title">Register yourself</span>
                 <form action="cust.php">
                     <div class="input-field">
-                        <input type="text" placeholder="Enter your name" required>
+                        <input type="text" placeholder="Enter your name" name="uname" required>
                         <i class="uil uil-user"></i>
                     </div>
                     <div class="input-field">
-                        <input type="text" placeholder="Enter your email" required>
+                        <input type="text" placeholder="Enter your email" name="email" required>
                         <i class="uil uil-envelope icon"></i>
                     </div>
                     <div class="input-field">
-                        <input type="password" class="password" placeholder="Create a password" required>
+                        <input type="password" class="password" placeholder="Create a password" name="password" required>
                         <i class="uil uil-lock icon"></i>
                     </div>
                     <div class="input-field">
-                        <input type="password" class="password" placeholder="Confirm a password" required>
-                        <i class="uil uil-lock icon"></i>
-                        <i class="uil uil-eye-slash showHidePw"></i>
+                        <input type="int" placeholder="Contact Number" name="contact" required>
+                        <i class="fa-solid fa-phone"></i>
                     </div>
-
-                    <div class="checkbox-text">
-                        <div class="checkbox-content">
-                            <input type="checkbox" id="termCon">
-                            <label for="termCon" class="text">I accepted all terms and conditions</label>
-                        </div>
-                    </div>
-
                     <div class="input-field button">
-                        <input type="button" value="Signup">
+                        <input type="submit" value="Signup">
                     </div>
                 </form>
-
                 <div class="login-signup">
                     <span class="text">Already a member?
                         <a href="#" class="text login-link">Login Now</a>
@@ -89,6 +74,8 @@
     </div>
 
     <script src="script.js"></script>
-
+    <script src="https://kit.fontawesome.com/96531cd29f.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 </body>
 </html>

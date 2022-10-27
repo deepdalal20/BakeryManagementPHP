497bc40b7711dae3680fe192581104b3b16f6caa
Deep Dalal <deepdalal20@gmail.com>
2022-10-27 21:24:03 +0530

    27/10/2022 (Order)

--- order.php
+++ order.php
@@ -1,10 +1,10 @@
 <?php
     include 'dbcon.php';
-    // session_start();
-    // if(!isset($_SESSION['loganame'])){
-    //     header('location:login.php');
-    // }
- 
+    session_start();
+    if(!isset($_SESSION['loganame'])){
+        header('location:login.php');
+    }
+    $u_id = $_SESSION['logaid'];
     $query = "select * from tblcart";
     $result= mysqli_query($conn, $query);
 
@@ -12,7 +12,7 @@
         $total=($row['crt_qty']* $row['crt_price']);
               $grandtotal += $total;
     endwhile;
-    $insert_product = "INSERT INTO `tblorder`(`o_total`, `date`) VALUES ('$grandtotal', current_timestamp())";
+    $insert_product = "INSERT INTO `tblorder`(`u_id`, `o_total`, `date`) VALUES ('$u_id', '$grandtotal', current_timestamp())";
     $icart = mysqli_query($conn, $insert_product);
 
     if($icart)
